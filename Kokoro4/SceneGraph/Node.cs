587d9cebfdedb58ea39041bff3345da927019d7b
Himanshu Goel <himanshu.goel2797@gmail.com>
2016-08-18 23:13:00 -0400

    Implemented a dirty flag to speed up updates

--- Kokoro4/SceneGraph/Node.cs
+++ Kokoro4/SceneGraph/Node.cs
@@ -9,7 +9,33 @@ namespace Kokoro.SceneGraph
 {
     public class Node
     {
-        public Matrix4 Transform { get; protected set; }
+        private bool _dirty;
+        private bool dirty
+        {
+            get { return _dirty; }
+            set
+            {
+                _dirty = value;
+
+                if (_dirty)
+                {
+                    Children.AsParallel().ForAll(a => a.dirty = true);
+                }
+            }
+        }
+        private Matrix4 _transform;
+        public Matrix4 Transform
+        {
+            get
+            {
+                return _transform;
+            }
+            protected set
+            {
+                dirty = (_transform != value);
+                _transform = value;
+            }
+        }
         public Matrix4 NetTransform { get; private set; }
 
         public Node Parent { get; set; }
@@ -24,13 +50,17 @@ namespace Kokoro.SceneGraph
             Transform = Matrix4.Identity;
             NetTransform = Transform;
             Name = name;
+            dirty = true;
         }
 
         public void UpdateTree()
         {
             //Update the current transform
-            if (Parent != null)
+            if (Parent != null && dirty)
+            {
                 NetTransform = Parent.NetTransform * Transform;
+                dirty = false;
+            }
 
             Children.AsParallel().ForAll(a => a?.UpdateTree());
         }
