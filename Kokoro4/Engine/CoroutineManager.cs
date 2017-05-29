48e29591b08fece29aec41e869eacecb87c23543
Himanshu Goel <hgoel0974@gmail.com>
2017-05-28 20:56:54 -0400

    Added CoroutineManager.

---
+++ Kokoro4/Engine/CoroutineManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kokoro.Engine
+{
+    public class CoroutineManager
+    {
+        Dictionary<string, Func<IEnumerable<object>>> coroutines;
+        Dictionary<string, IEnumerator<object>> states;
+
+        public CoroutineManager()
+        {
+            coroutines = new Dictionary<string, Func<IEnumerable<object>>>();
+            states = new Dictionary<string, IEnumerator<object>>();
+        }
+
+        public void AddCoroutine(Func<IEnumerable<object>> routine)
+        {
+            coroutines.Add(routine.GetType().Name, routine);
+        }
+
+        public void Invoke(string routine)
+        {
+            if (!coroutines.ContainsKey(routine))
+                throw new KeyNotFoundException();
+
+            if (!states.ContainsKey(routine))
+                states[routine] = coroutines[routine]().GetEnumerator();
+            else
+                states[routine].MoveNext();
+        }
+    }
+}
